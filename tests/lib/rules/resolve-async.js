a402422775cc9a97a09c05068e12abc1024fbe2a
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2015-07-25 12:25:07 -0500

    Adding VariableDeclarator logic and more unit tests.

--- tests/lib/rules/resolve-async.js
+++ tests/lib/rules/resolve-async.js
@@ -23,13 +23,69 @@ eslintTester.addRuleTest("lib/rules/resolve-async", {
         "test('name', function () { stop(); start(); });",
         "asyncTest('name', function () { start(); });",
         "QUnit.test('name', function () { stop(); start(); });",
-        "QUnit.asyncTest('name', function () { start(); });"
+        "QUnit.asyncTest('name', function () { start(); });",
+        "test('name', function (assert) { var done = assert.async(); done(); });",
+        "QUnit.test('name', function (assert) { var done = assert.async(); done(); });",
+        "test('name', function (assert) { var done; done = assert.async(); done(); });",
+        "QUnit.test('name', function (assert) { var done; done = assert.async(); done(); });"
     ],
 
     invalid: [
         {
             code: "asyncTest('name', function () {});",
-            errors: [{ message: "" }]
+            errors: [{
+                message: "Need 1 more start() calls",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "test('name', function () { stop(); });",
+            errors: [{
+                message: "Need 1 more start() calls",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "QUnit.asyncTest('name', function () {});",
+            errors: [{
+                message: "Need 1 more start() calls",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "QUnit.test('name', function () { stop(); });",
+            errors: [{
+                message: "Need 1 more start() calls",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "test('name', function (assert) { var done = assert.async(); });",
+            errors: [{
+                message: "Async callback \"done\" is not called",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "QUnit.test('name', function (assert) { var done = assert.async(); });",
+            errors: [{
+                message: "Async callback \"done\" is not called",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "test('name', function (assert) { var done; done = assert.async(); });",
+            errors: [{
+                message: "Async callback \"done\" is not called",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "QUnit.test('name', function (assert) { var done; done = assert.async(); });",
+            errors: [{
+                message: "Async callback \"done\" is not called",
+                type: "CallExpression"
+            }]
         }
     ]
 
