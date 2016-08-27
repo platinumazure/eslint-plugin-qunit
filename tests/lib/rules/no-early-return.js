5317698415159f2c79390c79f9b9d1cef2bf59e3
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2016-08-27 11:17:11 -0500

    Fix: no-early-return handles nested scopes (fixes #48)

--- tests/lib/rules/no-early-return.js
+++ tests/lib/rules/no-early-return.js
@@ -19,22 +19,65 @@ var rule = require("../../../lib/rules/no-early-return"),
 var ruleTester = new RuleTester();
 ruleTester.run("no-early-return", rule, {
     valid: [
-        "QUnit.test('a test', function (assert) { function foo() { return; } });",
-        "QUnit.test('a test', function (assert) { (function () { return; })(); });",
+        // Inside nested function (assertions outside of nested function)
+        "QUnit.test('a test', function (assert) { function foo() { return; } assert.ok(true); });",
+        "QUnit.test('a test', function (assert) { (function () { return; })(); assert.ok(true); });",
         {
-            code: "QUnit.test('a test', function (assert) { () => { return; }; });",
+            code: "QUnit.test('a test', function (assert) { () => { return; }; assert.ok(true); });",
             parserOptions: { ecmaVersion: 6 }
         },
-        "QUnit[shouldRunTest() ? 'test' : 'skip']('a test', function () {});",
-        "if (shouldRunTest()) { QUnit.test('a test', function () {}); }"
+
+        // Inside nested function (assertions inside nested function before return)
+        "QUnit.test('a test', function (assert) { function foo() { assert.ok(true); return; } });",
+        "QUnit.test('a test', function (assert) { (function () { assert.ok(true); return; })(); });",
+        {
+            code: "QUnit.test('a test', function (assert) { () => { assert.ok(true); return; }; });",
+            parserOptions: { ecmaVersion: 6 }
+        },
+
+        // Conditially run tests are okay
+        "QUnit[shouldRunTest() ? 'test' : 'skip']('a test', function (assert) { assert.ok(true); });",
+        "if (shouldRunTest()) { QUnit.test('a test', function (assert) { assert.ok(true); }); }"
     ],
 
     invalid: [
         {
-            code: "QUnit.test('a test', function () { return; });",
+            code: "QUnit.test('a test', function (assert) { if (true) return; assert.ok(true); });",
             errors: [{
                 message: "Do not return early from a QUnit test.",
                 type: "ReturnStatement"
+            }]
+        },
+
+        // Return in nested function before assertion is considered early return
+        {
+            code: "QUnit.test('a test', function (assert) { setTimeout(function () { if (true) return; assert.ok(true); }, 0); });",
+            errors: [{
+                message: "Do not return early from a QUnit test.",
+                type: "ReturnStatement"
+            }]
+        },
+        {
+            code: "QUnit.test('a test', function (assert) { setTimeout(function () { assert.ok(true); if (true) return; assert.ok(true); }, 0); });",
+            errors: [{
+                message: "Do not return early from a QUnit test.",
+                type: "ReturnStatement"
+            }]
+        },
+
+        // Report errors in multiple scopes with assertions
+        {
+            code: "QUnit.test('a test', function (assert) { setTimeout(function () { return; assert.ok(true); }, 0); return; assert.ok(true); });",
+            errors: [{
+                message: "Do not return early from a QUnit test.",
+                type: "ReturnStatement",
+                line: 1,
+                column: 67
+            }, {
+                message: "Do not return early from a QUnit test.",
+                type: "ReturnStatement",
+                line: 1,
+                column: 99
             }]
         }
     ]
