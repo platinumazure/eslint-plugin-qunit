23f858136d3b62c6c984673aa72a9778421a2975
em <ernesto.mudu@gmail.com>
2020-04-11 13:03:19 -0700

    New no-loose-asseritons rule.

---
+++ docs/rules/no-loose-assertions.md
@@ -0,0 +1,75 @@
+# Forbid the use of assert.equal/assert.ok/assert.notOk (no-loose-assertions)
+
+The `assert.equal` assertion method in QUnit uses loose equality comparison. In a project which favors strict equality comparison, it is better to use `assert.strictEqual` for scalar values and either `assert.deepEqual` or `assert.propEqual` for more complex objects.
+
+`assert.ok` and `assert.notOk` pass for any truthy/falsy argument. As [many expressions evaluate to true/false in JavaScript](https://developer.mozilla.org/en-US/docs/Glossary/Truthy) the usage of `assert.ok` is potentially error prone. In general, it should be advisable to always test for exact values in tests which makes tests a lot more solid.
+
+An example when using `assert.ok` can involuntarily go wrong:
+```
+test('test myFunc returns a truthy value' (assert) => {
+  assert.ok(myFunc);
+});
+```
+Here by mistake a developer just passed to `assert.ok` a pointer to `myFunc` instead of explicitly calling it. This test is going pass no matter how `myFunc` changes. Using `assert.strictEqual(myFunc, theReturnValue)` solves the problem as this becomes an explicit check for equality.
+
+The assertions to lint against can be controlled with an array of assertions (default `["equal", "ok", "notOk"]`).
+
+## Rule Details
+
+The following patterns are considered warnings:
+
+```js
+
+QUnit.test('Name', function (assert) { assert.ok(a); });
+
+QUnit.test('Name', function (foo) { foo.ok(a); });
+
+QUnit.test('Name', function () { ok(a); });
+
+QUnit.test('Name', function (assert) { assert.notOk(a); });
+
+QUnit.test('Name', function (foo) { foo.notOk(a); });
+
+QUnit.test('Name', function () { notOk(a); });
+
+QUnit.test('Name', function (assert) { assert.equal(a, b); });
+
+QUnit.test('Name', function (foo) { foo.equal(a, b); });
+
+QUnit.test('Name', function () { equal(a, b); });
+
+```
+
+The following patterns are not considered warnings:
+
+```js
+
+QUnit.test('Name', function (assert) { assert.strictEqual(a, true); });
+
+QUnit.test('Name', function (foo) { foo.strictEqual(a, true); });
+
+QUnit.test('Name', function () { strictEqual(a, true); });
+
+QUnit.test('Name', function (assert) { assert.strictEqual(a, false); });
+
+QUnit.test('Name', function (foo) { foo.strictEqual(a, false); });
+
+QUnit.test('Name', function () { strictEqual(a, false); });
+
+QUnit.test('Name', function (assert) { assert.deepEqual(a, b); });
+
+QUnit.test('Name', function (foo) { foo.deepEqual(a, b); });
+
+QUnit.test('Name', function () { deepEqual(a, b); });
+
+QUnit.test('Name', function (assert) { assert.propEqual(a, b); });
+
+QUnit.test('Name', function (foo) { foo.propEqual(a, b); });
+
+QUnit.test('Name', function () { propEqual(a, b); });
+
+```
+
+## Further Reading
+
+* [QUnit's Assertions](https://api.qunitjs.com/category/assert/)
---
+++ tests/lib/rules/no-loose-assertions.js
@@ -0,0 +1,274 @@
+/**
+ * @fileoverview Forbid the use of assert.equal/assert.ok/notOk and suggest other assertions.
+ * @author ventuno
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-loose-assertions"),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-loose-assertions", rule, {
+    valid: [
+        "QUnit.test('Name', function (assert) { assert.strictEqual(a, b); });",
+        "QUnit.test('Name', function (assert) { assert.deepEqual(a, b); });",
+        "QUnit.test('Name', function (assert) { assert.propEqual(a, b); });",
+        "QUnit.test('Name', function (foo) { foo.strictEqual(a, b); });",
+        "QUnit.test('Name', function (foo) { foo.deepEqual(a, b); });",
+        "QUnit.test('Name', function (foo) { foo.propEqual(a, b); });",
+        "QUnit.test('Name', function (assert) { strictEqual(a, b); });",
+        "QUnit.test('Name', function (assert) { deepEqual(a, b); });",
+        "QUnit.test('Name', function (assert) { propEqual(a, b); });",
+        "QUnit.test('Name', function () { strictEqual(a, b); });",
+        "QUnit.test('Name', function () { deepEqual(a, b); });",
+        "QUnit.test('Name', function () { propEqual(a, b); });",
+
+        // equal is not within test context
+        "equal(a, b);"
+    ],
+
+    invalid: [
+        {
+            code: "QUnit.test('Name', function (assert) { assert.ok(a); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "ok"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (foo) { foo.ok(a); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "ok"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (assert) { assert.notOk(a); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "notOk"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (foo) { foo.notOk(a); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "notOk"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (assert) { ok(a, b); });",
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "ok" }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (assert) { notOk(a, b); });",
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "notOk" }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function () { ok(a, b); });",
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "ok" }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function () { notOk(a, b); });",
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "notOk" }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (assert) { assert.equal(a, b); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (foo) { foo.equal(a, b); });",
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: "QUnit.test('Name', function (assert) { equal(a, b); });",
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "equal" }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function (assert) {
+                          assert.ok(a, b);
+                          assert.notOk(a, b);
+                          assert.equal(a, b);
+                      });
+                  `,
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "ok"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "notOk"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function (foo) {
+                          foo.ok(a, b);
+                          foo.notOk(a, b);
+                          foo.equal(a, b);
+                      });
+                  `,
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "ok"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "notOk"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function () {
+                          ok(a, b);
+                          notOk(a, b);
+                          equal(a, b);
+                      });
+                  `,
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "ok" }
+            }, {
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "notOk" }
+            }, {
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "equal" }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function (assert) {
+                          assert.ok(a, b);
+                          assert.notOk(a, b);
+                          assert.equal(a, b);
+                      });
+                  `,
+            options: [["ok", "equal"]],
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "ok"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "assert",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function (foo) {
+                          foo.ok(a, b);
+                          foo.notOk(a, b);
+                          foo.equal(a, b);
+                      });
+                  `,
+            options: [["ok", "equal"]],
+            errors: [{
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "ok"
+                }
+            }, {
+                messageId: "unexpectedLocalLooseAssertion",
+                data: {
+                    assertVar: "foo",
+                    assertion: "equal"
+                }
+            }]
+        },
+        {
+            code: `
+                      QUnit.test('Name', function () {
+                          ok(a, b);
+                          notOk(a, b);
+                          equal(a, b);
+                      });
+                  `,
+            options: [["ok", "equal"]],
+            errors: [{
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "ok" }
+            }, {
+                messageId: "unexpectedGlobalLooseAssertion",
+                data: { assertion: "equal" }
+            }]
+        }
+    ]
+});
