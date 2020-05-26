9d1026046f864d6ef9e2b15fa3f1d41015c8e43c
Ed Sanders <ejsanders@gmail.com>
2020-05-26 02:47:51 +0100

    Add a fixer for no-arrow-tests
    Fixer adapted from eslint-plugin-mocha (MIT)

--- tests/lib/rules/no-arrow-tests.js
+++ tests/lib/rules/no-arrow-tests.js
@@ -60,6 +60,7 @@ ruleTester.run("no-arrow-tests", rule, {
         // tests
         {
             code: "QUnit.test('test', (assert) => { assert.ok(true); });",
+            output: "QUnit.test('test', function(assert) { assert.ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -68,6 +69,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.test('test', () => { ok(true); });",
+            output: "QUnit.test('test', function() { ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -76,6 +78,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.asyncTest('test', (assert) => { assert.ok(true); });",
+            output: "QUnit.asyncTest('test', function(assert) { assert.ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -84,6 +87,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.asyncTest('test', () => { ok(true); });",
+            output: "QUnit.asyncTest('test', function() { ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -92,6 +96,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "test('test', (assert) => { assert.ok(true); });",
+            output: "test('test', function(assert) { assert.ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -100,6 +105,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "test('test', () => { ok(true); });",
+            output: "test('test', function() { ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -108,6 +114,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "asyncTest('test', (assert) => { assert.ok(true); });",
+            output: "asyncTest('test', function(assert) { assert.ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -116,6 +123,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "asyncTest('test', () => { ok(true); });",
+            output: "asyncTest('test', function() { ok(true); });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -126,6 +134,7 @@ ruleTester.run("no-arrow-tests", rule, {
         // modules
         {
             code: "QUnit.module('module', { setup: () => {} });",
+            output: "QUnit.module('module', { setup: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -134,6 +143,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.module('module', { teardown: () => {} });",
+            output: "QUnit.module('module', { teardown: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -142,6 +152,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.module('module', { beforeEach: () => {} });",
+            output: "QUnit.module('module', { beforeEach: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -150,6 +161,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "QUnit.module('module', { afterEach: () => {} });",
+            output: "QUnit.module('module', { afterEach: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -158,6 +170,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "module('module', { setup: () => {} });",
+            output: "module('module', { setup: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -166,6 +179,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "module('module', { teardown: () => {} });",
+            output: "module('module', { teardown: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -174,6 +188,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "module('module', { beforeEach: () => {} });",
+            output: "module('module', { beforeEach: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
@@ -182,6 +197,7 @@ ruleTester.run("no-arrow-tests", rule, {
         },
         {
             code: "module('module', { afterEach: () => {} });",
+            output: "module('module', { afterEach: function() {} });",
             parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noArrowFunction",
