88122a98b8625a8a73ad52cd8ac4fee320cce8f7
Alex Y <aliaksandr.yermalayeu@gmail.com>
2020-08-20 17:01:20 +0300

    Chore: Added more test cases, changed description

--- docs/rules/no-nested-tests.md
+++ docs/rules/no-nested-tests.md
@@ -16,6 +16,12 @@ test('Parent', function () {
     test('Child', function () {});
 });
 
+QUnit.test('ParentTest', function () {
+    QUnit.module('ChildModule', function () {
+        QUnit.test('ChildTest', function () {});
+    });
+});
+
 ```
 
 The following patterns are not considered warnings:
@@ -35,7 +41,7 @@ QUnit.module('ParentModule', function () {
 
 ```
 
-## When Not To Use It
+## When Not to Use It
 
 It should be safe to use this rule. However it may cause false positive when using same namespace `test` during act or arrange stages.
 
--- tests/lib/rules/no-nested-tests.js
+++ tests/lib/rules/no-nested-tests.js
@@ -23,26 +23,30 @@ ruleTester.run("no-nested-tests", rule, {
         "test('Name', function() { });",
         "QUnit.test('First', function() { }); \n QUnit.test('Second', function() { });",
         "test('First', function() { }); \n test('Second', function() { });",
+        "QUnit.test('First', function() { }); \n test('Second', function() { });",
+        "test('First', function() { }); \n QUnit.test('Second', function() { });",
         "QUnit.module('ParentModule', function () {\n QUnit.test('Name', function() {});\n QUnit.module('ChildModule', function () {\n QUnit.test('ChildTest', function () {});\n });\n })\n",
+        "QUnit.module('ParentModule', function () {\n test('Name', function() {});\n module('ChildModule', function () {\n QUnit.test('ChildTest', function () {});\n });\n })\n",
+        "module('ParentModule', function () {\n QUnit.test('Name', function() {});\n QUnit.module('ChildModule', function () {\n test('ChildTest', function () {});\n });\n })\n",
         "module('ParentModule', function () {\n test('Name', function() {});\n module('ChildModule', function () {\n test('ChildTest', function () {});\n });\n })\n"
     ],
 
     invalid: [
-        {
-            code: "QUnit.test('Parent', function() {\n QUnit.test('Child', function () {}); });",
-            errors: [
-                {
-                    messageId: "noNestedTests"
-                }
-            ]
-        },
-        {
-            code: "test('Parent', function() {\n test('Child', function () {}); });",
-            errors: [
-                {
-                    messageId: "noNestedTests"
-                }
-            ]
-        }
+        "QUnit.test('Parent', function() {\n QUnit.test('Child', function () {}); });",
+        "test('Parent', function() {\n test('Child', function () {}); });",
+        "QUnit.test('Parent', function() {\n test('Child', function () {}); });",
+        "test('Parent', function() {\n QUnit.test('Child', function () {}); });",
+        "QUnit.test('ParentTest', function () {\n QUnit.module('ChildModule', function () {\n QUnit.test('ChildTest', function () {});\n });\n });",
+        "test('ParentTest', function () {\n module('ChildModule', function () {\n test('ChildTest', function () {});\n });\n });",
+        "QUnit.test('ParentTest', function () {\n module('ChildModule', function () {\n test('ChildTest', function () {});\n });\n });",
+        "test('ParentTest', function () {\n QUnit.module('ChildModule', function () {\n QUnit.test('ChildTest', function () {});\n });\n });"
     ]
+        .map(code => ({
+            code,
+            errors: [
+                {
+                    messageId: "noNestedTests"
+                }
+            ]
+        }))
 });
