51cd143b97234d8234672fa04c73b8d03fd20aad
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-12-10 18:07:38 -0500

    feat: support eslint flat config

--- index.js
+++ index.js
@@ -8,14 +8,24 @@
 "use strict";
 
 const requireIndex = require("requireindex");
+const pkg = require("./package.json");
 
 module.exports = {
+    meta: {
+        name: pkg.name,
+        version: pkg.version
+    },
+
     rules: requireIndex(`${__dirname}/lib/rules`),
 
     // eslint-disable-next-line sort-keys
     configs: {
         recommended: {
-            description: "This configuration includes rules which I recommend to avoid QUnit runtime errors or incorrect behavior, some of which can be difficult to debug. Some of these rules also encourage best practices that help QUnit work better for you. You can use this configuration by extending from `\"plugin:qunit/recommended\"` in your configuration file.",
+            description: [
+                "This configuration includes rules which I recommend to avoid QUnit runtime errors or incorrect behavior, some of which can be difficult to debug. Some of these rules also encourage best practices that help QUnit work better for you.",
+                "For ESLint `.eslintrc.js` legacy config, extend from `\"plugin:qunit/recommended\"`.",
+                "For ESLint `eslint.config.js` flat config, load from `require('eslint-plugin-qunit/configs/recommended')`."
+            ].join(" "),
             plugins: ["qunit"],
             rules: {
                 "qunit/assert-args": "error",
